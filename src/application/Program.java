97ad28f71d7c801baf4ab48137ebf7e4a50b4dfb
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-05-10 03:03:02 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,14 +14,13 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class Program {
-	public static void main(String[] args) {
-		Department dp = new Department(1, "Books");
-		Seller seller = new Seller(22, "Bob", "bob@gmail.com", new Date(), 3000.00, dp);
-		
-		System.out.println(dp);
-		System.out.println(seller);
+public class Program 
+{
+	public static void main(String[] args) 
+	{
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import database.DataSource;
 import model.dao.implement.SellerDaoJDBC;
 
 public class DaoFactory 
 {
 	public static SellerDao createSellerDao() 
 	{
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DataSource.getConnection());
 	}
 }
