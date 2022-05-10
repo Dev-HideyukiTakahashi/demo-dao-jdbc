433d20a12552fb38e80bda0e26d9af0fbb33e1c6
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-05-10 02:26:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,23 @@
+/*
+ * Application made for database studies with JDBC
+ * Student: Hideyuki Takahashi
+ * Date: 10/05/2022
+ * Professor: Nelio Alves / DevSuperior
+ */
+
 package application;
 
-import dao.DataSource;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department dp = new Department(1, "Books");
+		Seller seller = new Seller(22, "Bob", "bob@gmail.com", new Date(), 3000.00, dp);
 		
 		System.out.println(dp);
+		System.out.println(seller);
 	}
 }
