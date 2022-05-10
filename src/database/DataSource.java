b24a36d396be14f8bb63d61d847f75e46ccc09ed
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-05-10 02:34:09 -0300

    DepartmentDao and SellerDao interfaces

--- src/database/DataSource.java
+++ src/database/DataSource.java
@@ -1,4 +1,4 @@
-package dao;
+package database;
 
 import java.io.FileInputStream;
 import java.io.IOException;
--- src/database/DataSourceException.java
+++ src/database/DataSourceException.java
@@ -1,4 +1,4 @@
-package dao;
+package database;
 
 public class DataSourceException extends RuntimeException {
 
